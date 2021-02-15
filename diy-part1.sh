938af3b6df4389bc400da528da78f1bd582563be
pop098 <65938943+pop098@users.noreply.github.com>
2021-02-15 21:35:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-
-git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
